1cdfcc3e5e8df826b269ae747d178a373dae564d
Pramila15 <53640792+Pramila15@users.noreply.github.com>
2021-06-28 15:18:49 +0530

    reqparse

--- items/app.py
+++ items/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -25,6 +25,13 @@ jwt = JWT(app, authenticate, identity)
 items = []
 # GET /items
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field should be filled"
+        )
+
     #authenticate before we call get
     # 401 - unauthorized
     # Header - Authorization - JWT paste_JST_TOKEN
@@ -44,6 +51,13 @@ class Item(Resource):
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('price',
+        # type=float,
+        # required=True,
+        # help="This field should be filled"
+        # )
+
         #we need json payload
         # header -> content type -> json
         #look into content and check json type (force=True)
@@ -53,10 +67,32 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name , 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted succesfully'}
+
+    def put(self, name):
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('price',
+        # type=float,
+        # required=True,
+        # help="This field should be filled"
+        # )
+        data = Item.parser.parse_args()
+        #data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
@@ -74,3 +110,5 @@ app.run(port=5000, debug=True)
 #pip3 install Flask-JWT
 
 # JWT - Json web token - encoding some data
+
+# reqparse - see which args match
