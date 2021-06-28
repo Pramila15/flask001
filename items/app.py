cc5ca6376ce7572d7c1039bda2eba895fc35f6f6
Pramila15 <53640792+Pramila15@users.noreply.github.com>
2021-06-28 12:06:21 +0530

    jwt

--- items/app.py
+++ items/app.py
@@ -1,13 +1,34 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'prams'
+# production api - keep it secret
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+#JWT creates new endpoint /auth
+# we save username and password
+# sends to authenticate
+# find correct user obj
+# then compare 
+
+# /auth endpoint return jwt token
+# we can send it to next req
+# calls the identity func 
+# get userid , that jwt token represents
 
 #2 resources
 items = []
 # GET /items
 class Item(Resource):
+    #authenticate before we call get
+    # 401 - unauthorized
+    # Header - Authorization - JWT paste_JST_TOKEN
+    @jwt_required()
     def get(self, name):
         # for item in items:
         #     if item['name'] == name:
@@ -31,7 +52,7 @@ class Item(Resource):
 
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-            
+
         data = request.get_json()
         item = {'name': name , 'price': data['price']}
         items.append(item)
@@ -49,3 +70,7 @@ app.run(port=5000, debug=True)
 # POST /item/<name>
 # DELETE /item/<name>
 # PUT /item/<name>
+
+#pip3 install Flask-JWT
+
+# JWT - Json web token - encoding some data
