6a0e08851becd8ba8c3a87e56f1aaa10a16a43ba
Pramila15 <53640792+Pramila15@users.noreply.github.com>
2021-07-07 11:10:26 +0530

    delete item

--- sql database/app.py
+++ sql database/app.py
@@ -1,63 +1,117 @@
 #!/usr/bin/env python3
-from flask import Flask
-from flask_restful import Api
-from flask_jwt import JWT
+import sqlite3
+from sqlite3.dbapi2 import connect
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
 
-from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field should be filled"
+        )
 
-app = Flask(__name__)
-app.secret_key = 'prams'
-api = Api(app)
+    @jwt_required()
+    def get(self, name):
+        # item = next(filter(lambda x: x['name'] == name, items), None)
+        # return {'item': item}, 200 if item else 404
+        #------#
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
 
-jwt = JWT(app, authenticate, identity)
+        # query = "SELECT * FROM items WHERE name=?"
+        # result = cursor.execute(query, (name,))
+        # row = result.fetchone()
+        # connection.close()
 
-# items = []
+        # if row:
+        #     return {'item': row[0], 'price': row[1]}
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
 
-# class Item(Resource):
-#     parser = reqparse.RequestParser()
-#     parser.add_argument('price',
-#         type=float,
-#         required=True,
-#         help="This field should be filled"
-#         )
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
 
-#     @jwt_required()
-#     def get(self, name):
-#         item = next(filter(lambda x: x['name'] == name, items), None)
-#         return {'item': item}, 200 if item else 404
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
 
-#     def post(self, name):
-#         if next(filter(lambda x: x['name'] == name, items), None) is not None:
-#             return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        if row:
+            return {'item': row[0], 'price': row[1]}
 
-#         data = Item.parser.parse_args()
-#         item = {'name': name , 'price': data['price']}
-#         items.append(item)
-#         return item, 201
+    def post(self, name):
+        # if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        #     return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
-#     def delete(self, name):
-#         global items
-#         items = list(filter(lambda x: x['name'] != name, items))
-#         return {'message': 'Item deleted succesfully'}
+        # data = Item.parser.parse_args()
+        # item = {'name': name , 'price': data['price']}
+        # items.append(item)
+        # return item, 201
+        if Item.find_by_name(name):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
 
-#     def put(self, name):
-#         data = Item.parser.parse_args()
-#         item = next(filter(lambda x: x['name'] == name, items), None)
-#         if item is None:
-#             item = {'name': name, 'price': data['price']}
-#             items.append(item)
-#         else:
-#             item.update(data)
-#         return item
+        data = Item.parser.parse_args()
 
-# class ItemList(Resource):
-#     def get(self):
-#         return {'items': items}
+        item = {
+            'name': name,
+            'price': data['price']
+        }
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
 
-api.add_resource(Item, '/item/<name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
 
-app.run(port=5000, debug=True)
+        connection.commit()
+        connection.close()
+
+        return item, 201
+
+    def delete(self, name):
+        # global items
+        # items = list(filter(lambda x: x['name'] != name, items))
+        # return {'message': 'Item deleted succesfully'}
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted succesfully'}
+
+    # def put(self, name):
+    #     data = Item.parser.parse_args()
+    #     item = next(filter(lambda x: x['name'] == name, items), None)
+    #     if item is None:
+    #         item = {'name': name, 'price': data['price']}
+    #         items.append(item)
+    #     else:
+    #         item.update(data)
+    #     return item
+
+class ItemList(Resource):
+    # def get(self):
+    #     return {'items': items}
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
+        return {'items': items}
